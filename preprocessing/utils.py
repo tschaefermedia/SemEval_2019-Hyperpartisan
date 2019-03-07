05e6401e1ff6cad0dd65299865eaac63cb12d805
Tobias Schäfer <tobias.schaefer@media-t.de>
2019-03-07 17:53:16 +0100

    Update utils

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -1,19 +1,17 @@
-import itertools
+import string
 
 import pandas as pd
 import xml.etree.ElementTree as ET
-import io
 import re
+from nltk.corpus import stopwords
 
+pd.set_option('display.max_columns', None)
 
 def content(element):
     return (element.text or '') + ''.join(ET.tostring(e, 'unicode') for e in element)
 
 
 def iter_data(data):
-    nprocessed = 0
-    all = 0
-    debug = True
     tree = ET.iterparse(data)
     for event, element in tree:
         if element.tag == "article":
@@ -23,33 +21,63 @@ def iter_data(data):
             yield doc_dict
 
 
-def remove_links(df):
+def clean_content(df):
     for index, row in df.iterrows():
         cleaner = re.compile('<.*?>')
-        cleantext = re.sub(cleaner, '', row['content'])
-        row['content'] = cleantext
+        cleaned_text = re.sub(cleaner, '', row['content'])
+        row['content'] = cleaned_text
+        row['content'] = " ".join(clean_text(row['content']))
     return df
 
 
+def clean_text(text):
+        '''
+        What will be covered:
+        1. Remove punctuation
+        2. Remove stopwords
+        3. Return list of clean text words
+        '''
+
+        # 1
+        nopunc = [char for char in text if char not in string.punctuation]
+        nopunc = ''.join(nopunc)
+
+        # 2
+        clean_words = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+
+        # 3
+        return clean_words
 
 
 def iter_truth(data):
-    pass
+    tree = ET.iterparse(data)
+    for event, element in tree:
+        if element.tag == "article":
+            doc_dict = element.attrib
+            yield doc_dict
 
 
 def parse_xml_files(data, truth, save, outfile):
     columns_data = ['id', 'published-at', 'title', 'content']
-    columns_truth = ['hyp', 'bias', 'url']
+    columns_truth = ['id', 'hyp', 'bias', 'url']
     bias = {'left': -1, 'left-center': -0.5, 'least': 0, 'right-center': 0.5, 'right': 1}
     data_df = pd.DataFrame(columns=columns_data)
-
+    truth_df = pd.DataFrame(columns=columns_truth)
     for file in data:
         with open(file, encoding="utf8") as f:
             print("Starting with file: " + file)
             df = pd.DataFrame.from_records(list(iter_data(f)))
             data_df = data_df.append(df, sort=True)
             print("Finished file: " + file)
-    data_df = remove_links(data_df)
+    data_df = clean_content(data_df)
+    for file in truth:
+        with open(file, encoding="utf8") as f:
+            print("Starting with file: " + file)
+            df = pd.DataFrame.from_records(list(iter_truth(f)), columns=columns_truth)
+            truth_df = truth_df.append(df, sort=True)
+            print("Finished file: " + file)
+    print(data_df.head())
+    print(truth_df.head())
     if save:
         data_df.to_csv(outfile)
 
